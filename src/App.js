56b07f43b8ac42d10478d91ed57840c2f8d958de
Agora243 <82520645+Agora243@users.noreply.github.com>
2021-05-31 16:15:43 +0100

    App Js rend le router

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import FormConnexion from './components/formConnexion/formConnexion';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import HomeSearch from './pages/homeSearch/homeSearch';
+import Router from './router';
+import { Fragment } from 'react';
 
 function App() {  
-  {var route =  
-    <BrowserRouter>
-      <Switch>
-        <Route path ='/login'>
-          <FormConnexion />
-        </Route>
-        <Route path ='/'>
-          <HomeSearch />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  };
-
-  return (
-    <div>
-      {route}
-      <h2><a href='login'>Login</a></h2>
-    </div>
+ 
+  return <Router />;
   
-  );
 }
 
 export default App;
